3a3bdd5fe1575c0c826f331e763eed544fca9142
miguel tostado <mtalamo2000@gmail.com>
2021-06-16 18:56:15 -0500

    Git merge y creacion del modelo inicial de los archivos

--- src/controllers/controller_archivo.js
+++ src/controllers/controller_archivo.js
@@ -1,5 +1,7 @@
 const archivoCtrl = {};
 
+const ModeloArchivo = require("../models/model_archivo.js");
+
 const AWS = require('aws-sdk').S3;
 
 const id = process.env.ID_S3;
@@ -14,13 +16,24 @@ const s3 = new AWS({
 
 archivoCtrl.guardarBucket = async (req, res) => {
 
-  const params = { Bucket: bucket, Key: `${req.params.id}/${req.file.originalname}`, Body: req.file.path };
+  let keyName = `${req.params.id}/${req.file.originalname}`
+
+  const params = { Bucket: bucket, Key: keyName, Body: req.file.path };
 
   s3.upload(params, function(err, data) {
     if (err) {
       res.json({message: 'Ocurrio un error en el programa:'}, err)
     }
   });
+
+  const nuevoArchivo = new ModeloArchivo({
+    key: keyName,
+    usuarioId: req.params.id,
+    type: req.file.mimetype,
+  });
+
+  await nuevoArchivo.save();
+
 
   res.json({ message: 'Guardado con exito! '});
 };
---
+++ src/models/model_archivo.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require("mongoose");
+
+const archivoSchema = new Schema({
+  usuarioId: {
+    type: String,
+    required: true,
+  },
+  key: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = model("archivos", archivoSchema);
